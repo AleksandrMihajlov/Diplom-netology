ae23fbaf6925366e60ae76bec2a229b9686e7b46
aleksandrmihajlov <misashok@yandex.ru>
2024-12-23 22:20:41 +0300

    com1

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "token" {
+  type = string
+  description = "ID for YaCloud"
+}
+
+variable "cloud_id" {
+  default = "**********************"
+}
+
+variable "folder_id" {
+  default = "**********************"
+}
